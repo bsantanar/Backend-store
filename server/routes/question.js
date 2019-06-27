7d60b3b7b01f72c679f497416fde14f0b31f198f
Bastian Santana <bsantana17@gmail.com>
2019-06-27 02:11:57 -0400

    save different questions

--- server/routes/question.js
+++ server/routes/question.js
@@ -24,12 +24,26 @@ app.get('/questions', (req, res) => {
 
 app.post('/questions', (req, res) => {
     let body = req.body;
-
+    //console.log(body);
     let question = new Question({
         title: body.title,
         questionId: body.questionId,
-        type: body.type
+        type: body.type,
+        required: body.required,
+        hint: body.hint
     });
+
+    if (body.options) {
+        question.set('options', body.options);
+    } else if (body.type == "scale") {
+        question.set('min', body.min);
+        question.set('max', body.max);
+        question.set('step', body.step);
+        question.set('minLabel', body.minLabel);
+        question.set('maxLabel', body.maxLabel);
+    } else if (body.maxStars) {
+        question.set('maxStars', body.maxStars);
+    }
 
     question.save((err, questionDB) => {
         if (err) {
